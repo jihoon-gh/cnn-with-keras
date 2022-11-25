8be44e1618a0d0686c9e0713e3dcf30883d75080
jihoon-gh <jihoon806@gmail.com>
2022-11-25 19:10:01 +0900

    FEAT - Add early_stopping and ModelCheckPoint

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from keras import models
 from keras.layers import *
+from keras.callbacks import ModelCheckpoint,EarlyStopping
 import matplotlib.pyplot as plt
 from dataset.fruit import load_fruit
 import plot_show
@@ -20,25 +21,24 @@ x_test = x_test / 255.
 model = models.Sequential()
 model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(128, 104, 3), strides=(1, 1), padding='same'))
 model.add(Conv2D(32, (3, 3), activation='relu', strides=(1, 1), padding='same'))
-model.add(Conv2D(32, (3, 3), activation='relu', strides=(1, 1), padding='same'))
 model.add(MaxPooling2D((2, 2)))
-model.add(Dropout(0.25))
+model.add(Dropout(0.2))
 
 model.add(Conv2D(64, (3, 3), activation='relu', strides=(1, 1), padding='same'))
 model.add(Conv2D(64, (3, 3), activation='relu', strides=(1, 1), padding='same'))
 model.add(Conv2D(64, (3, 3), activation='relu', strides=(1, 1), padding='same'))
 model.add(MaxPooling2D((2, 2)))
-model.add(Dropout(0.25))
+model.add(Dropout(0.2))
 
 model.add(Conv2D(128, (3, 3), activation='relu', strides=(1, 1), padding='same'))
 model.add(Conv2D(128, (3, 3), activation='relu', strides=(1, 1), padding='same'))
 model.add(Conv2D(128, (3, 3), activation='relu', strides=(1, 1), padding='same'))
 model.add(MaxPooling2D((2, 2)))
-model.add(Dropout(0.25))
+model.add(Dropout(0.2))
 
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.25))
+model.add(Dropout(0.2))
 model.add(Dense(10, activation='softmax'))
 
 
@@ -46,11 +46,15 @@ model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
+mc = ModelCheckpoint('best_model.h5', monitor='val_loss', mode='min', save_best_only=True)
+early_stopping = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=10)
+
 model.summary()
 history = model.fit(x_train, t_train,
                     batch_size=200,
                     validation_data=(x_test, t_test),
                     epochs=10,
+                    callbacks=[early_stopping,mc],
                     # steps_per_epoch=len(x_train),
                     # validation_steps=len(x_test)
                     )
